cd9ef420d4e63dd5d3a167a0eb884b21e76f7a59
ESP32 - ESP8266 para Tec.Dig. UA <125473983+jtoobe@users.noreply.github.com>
2024-10-09 15:02:40 -0300

    Create ESP32_mp3_fast.ino

---
+++ ESP32_mp3_fast.ino
@@ -0,0 +1,42 @@
+// Tomado del ejemplo de la libreria DFPlayerMini_Fast
+
+// hay que instalar la libreria ESPSoftwareSerial antes de poder compilar
+
+
+#include <DFPlayerMini_Fast.h>
+
+#if !defined(UBRR1H)
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(27, 26); // RX, TX
+#endif
+
+DFPlayerMini_Fast myMP3;
+
+void setup()
+{
+  Serial.begin(115200);
+
+#if !defined(UBRR1H)
+  mySerial.begin(9600);
+  myMP3.begin(mySerial, true);
+#else
+  Serial1.begin(9600);
+  myMP3.begin(Serial1, true);
+#endif
+  
+  delay(1000);
+  
+  Serial.println("Setting volume to max");
+  myMP3.volume(30);  // mas de 30 no va.
+  
+  Serial.println("Looping track 1");
+
+  // reproduce el PRIMER archivo mp3 que encuentra en el directorio raiz de la SD
+  
+  myMP3.loop(1);
+}
+
+void loop()
+{
+  //do nothing
+}
